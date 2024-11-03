f568a5bdaac9a9da0ae5e52c4f3eb013d288d110
GimiOTR <arionpetrela@gmail.com>
2024-11-03 04:43:42 +0100

    feat: configure entity relationships and add migration

--- EIS-backend/EIS.Infrastructure/Data/ApplicationDbContext.cs
+++ EIS-backend/EIS.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,46 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EIS.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EIS.Infrastructure.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudyProgram> Programs { get; set; }
+        public DbSet<CourseProgram> CoursePrograms { get; set; }
+        public DbSet<CourseProgramYear> CourseProgramLecturers { get; set; }
+        public DbSet<AcademicYear> AcademicYears { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<CourseProgram>()
+                .HasKey(cp => new { cp.CourseId, cp.ProgramId });
+
+            builder.Entity<CourseProgram>()
+                .HasOne(cp => cp.Course)
+                .WithMany(c => c.CoursePrograms)
+                .HasForeignKey(cp => cp.CourseId);
+
+            builder.Entity<CourseProgram>()
+                .HasOne(cp => cp.Program)
+                .WithMany(p => p.CoursePrograms)
+                .HasForeignKey(cp => cp.ProgramId);
+
+            builder.Entity<CourseProgramYear>()
+                .HasKey(cpl => new { cpl.CourseId, cpl.ProgramId, cpl.AcademicYearId });
+
+            builder.Entity<CourseProgramYear>()
+                .HasOne(cpl => cpl.CourseProgram)
+                .WithMany(cp => cp.CourseProgramYears)
+                .HasForeignKey(cpl => new { cpl.CourseId, cpl.ProgramId });
+
+            builder.Entity<CourseProgramYear>()
+                .HasOne(cpl => cpl.AcademicYear)
+                .WithMany(ay => ay.CourseProgramYears)
+                .HasForeignKey(cpl => cpl.AcademicYearId);
+        }
     }
 }
