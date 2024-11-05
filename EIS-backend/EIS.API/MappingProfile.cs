5bfce8fa47fe029786cdf3e1d7011304d65a2dfa
GimiOTR <arionpetrela@gmail.com>
2024-11-05 23:24:01 +0100

    feat: add mapping profile, BaseResponse and Exceptions

---
+++ EIS-backend/EIS.API/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EIS.Application.DTO.Request;
+using EIS.Application.DTO.Response;
+using EIS.Domain.Entities;
+
+namespace EIS.API
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Course
+            CreateMap<Course,CourseRequestDTO>().ReverseMap();
+            CreateMap<CourseResponseDTO,Course>().ReverseMap();
+
+            //Program
+            CreateMap<StudyProgram, ProgramRequestDTO>().ReverseMap();
+            CreateMap<ProgramResponseDTO, StudyProgram>().ReverseMap();
+        }
+    }
+}
