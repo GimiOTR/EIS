c4ee8a6f098b0a27900cd6dfa4b19794c4f6666f
GimiOTR <arionpetrela@gmail.com>
2024-11-07 23:47:16 +0100

    feat: add migration and trigger in db context

---
+++ EIS-backend/EIS.Infrastructure/Data/Migrations/20241105215814_ModifiedAcademicYear.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EIS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedAcademicYear : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FallSemesterFinalized",
+                table: "AcademicYears",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SpringSemesterFinalized",
+                table: "AcademicYears",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicYears_EndYear",
+                table: "AcademicYears",
+                column: "EndYear",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicYears_StartYear",
+                table: "AcademicYears",
+                column: "StartYear",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AcademicYears_EndYear",
+                table: "AcademicYears");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AcademicYears_StartYear",
+                table: "AcademicYears");
+
+            migrationBuilder.DropColumn(
+                name: "FallSemesterFinalized",
+                table: "AcademicYears");
+
+            migrationBuilder.DropColumn(
+                name: "SpringSemesterFinalized",
+                table: "AcademicYears");
+        }
+    }
+}
